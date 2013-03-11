b5f71b56d50583084294e9e1d35c71dde53eadff
Manish Gill <gill.manish90@gmail.com>
2013-03-11 16:04:35 +0530

    setup.py modifications

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import os
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-requirements = []
+from setuptools import setup, find_packages
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def get_requirements():
+    return [elem.strip() for elem in
+            read('requirements.txt').splitlines()
+            if elem.strip() and not elem.startswith('#')]
 
 config = {
     'name': 'CSS Cleaner',
@@ -18,8 +18,9 @@ config = {
     'author': 'Tarun Arora and Manish Gill',
     'url': 'https://github.com/gobelinus/csscleaner',
     'version': '0.0.1',
-    'install_requires': requirements,
-    'packages': ['csscleaner', 'tests'],
+    'license': 'GPL',
+    'install_requires': get_requirements(),
+    'packages': find_packages(),
     'scripts': [],
     'long_description': read('README.md')
 }
